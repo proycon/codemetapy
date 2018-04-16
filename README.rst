44a5930a86432b06d4fa7f09ef67a851ab55e0d0
Maarten van Gompel <proycon@anaproy.nl>
2018-04-16 13:58:16 +0200

    added initial documentation and setup

---
+++ README.rst
@@ -0,0 +1,28 @@
+CodeMetaPy
+=================
+
+The goal of CodeMetaPy is to generate the JSON-LD file, codemeta.json containing software metadata describing a Python
+package. For more general information about the CodeMeta Project for defining software metadata, see
+https://codemeta.github.io. In particular, new users might want to start with the User Guide, while those looking to
+learn more about JSON-LD and consuming existing codemeta files should see the Developer Guide.
+
+Installation
+----------------
+
+``pip install codemetapy``
+
+Usage
+---------------
+
+Query and convert any package installed through pip:
+
+```
+$ pip show -v somepackage | codemetadapy
+```
+
+To pipe to an output file:
+
+```
+$ pip show -v somepackage | codemetadapy > somepackage.json
+```
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+from __future__ import print_function
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname),'r',encoding='utf-8').read()
+
+setup(
+    name = "CodeMetaPy",
+    version = "0.1",
+    author = "Maarten van Gompel",
+    author_email = "proycon@anaproy.nl",
+    description = ("Generate CodeMeta metadata for Python packages"),
+    license = "GPL",
+    keywords = "nlp computational_linguistics entities wikipedia dbpedia linguistics tramooc",
+    url = "https://github.com/proycon/codemeta",
+    packages=['codemeta'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Text Processing :: Linguistic",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    package_data = { },
+    install_requires=[ 'nameparser', 'json' ],
+    entry_points = {    'console_scripts': [ 'codemetapy = codemetapy.codemeta:main' ] }
+)
