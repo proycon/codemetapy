d4dc0639086a8cae6e43846d6a875a7e229dfa90
Maarten van Gompel <proycon@anaproy.nl>
2018-04-17 21:13:05 +0200

    fix

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     ],
     zip_safe=False,
     include_package_data=True,
-    package_data = { 'codemeta': 'schema/*' },
+    package_data = { 'codemeta': ['schema/*'] },
     install_requires=[ 'nameparser'  ],
     entry_points = {    'console_scripts': [ 'codemetapy = codemeta.codemeta:main' ] }
 )
