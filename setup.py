776fa4162734ea5133837e978d292b03971b95ef
Maarten van Gompel <proycon@anaproy.nl>
2018-04-17 21:41:33 +0200

    packages only needed data

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     ],
     zip_safe=False,
     include_package_data=True,
-    package_data = { 'codemeta': ['schema/*'] },
+    package_data = { 'codemeta': ['schema/crosswalk.csv', 'schema/codemeta.jsonld'] },
     install_requires=[ 'nameparser'  ],
     entry_points = {    'console_scripts': [ 'codemetapy = codemeta.codemeta:main' ] }
 )
