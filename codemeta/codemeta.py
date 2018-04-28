845f701807d0dfa94ae55c905830bb27ead044b0
Maarten van Gompel <proycon@anaproy.nl>
2018-04-28 14:22:14 +0200

    fix

--- codemeta/codemeta.py
+++ codemeta/codemeta.py
@@ -232,7 +232,7 @@ def main():
             if len(args.input.split(",")) != len(args.inputfiles):
                 print("Passed " + str(len(args.inputfiles)) + " files but specified only " + str(len(args.input)) + " input types!",file=sys.stderr)
             else:
-                inputfiles = [ (open(f,'r',encoding='utf-8'), t) if f != '-' and t != 'registry' else (sys.stdin,t) for f,t in zip(args.inputfiles, args.input.split(',')) ]
+                inputfiles = [ (open(f,'r',encoding='utf-8'), t) if f != '-' and t != 'registry' else ( (f,t) if t == 'registry' else (sys.stdin,t) ) for f,t in zip(args.inputfiles, args.input.split(',')) ]
         else:
             inputfiles = [ (open(f,'r',encoding='utf-8'), args.input) if f != '-' else (sys.stdin,args.input) for f in args.inputfiles ] #same type for all
     else:
@@ -252,7 +252,7 @@ def main():
     })
     for stream, inputtype in inputfiles:
         if inputtype == "registry":
-            data = getregistry(inputtype, registry)
+            data = getregistry(stream, registry)
         elif inputtype == "pip":
             piplines = stream.read().split("\n")
             data = parsepip(data, piplines, mapping, args.with_entrypoints)
