4f23f78bfd2c382eebfc65ad7e04e199eb6e96de
Maarten van Gompel <proycon@anaproy.nl>
2022-03-06 00:05:42 +0100

    Fix for merging multiple json files #12

--- codemeta/parsers/jsonld.py
+++ codemeta/parsers/jsonld.py
@@ -1,3 +1,4 @@
+import sys
 import json
 from rdflib import Graph, URIRef, BNode, Literal
 from typing import Union, IO
@@ -38,16 +39,24 @@ def parse_jsonld(g: Graph, res: Union[BNode, URIRef,None], file_descriptor: IO, 
     if isinstance(res, URIRef):
         if '@graph' in data and len(data['@graph']) == 1:
             #force same ID as the resource (to facilitate merging), but return the preferred URI to be used on serialisation again
+            found = False
             for k in ('id','@id'):
                 if k in data['@graph'][0]:
                     prefuri = data['@graph'][0][k]
                     data[k] = str(res)
+            if not found:
+                data['@graph'][0]["@id"] = str(res)
         elif '@id' in data or 'id' in data:
             #force same ID as the resource (to facilitate merging), but return the preferred URI to be used on serialisation again
+            found = False
             for k in ('id','@id'):
                 if k in data:
                     prefuri = data[k]
                     data[k] = str(res)
+            if not found:
+                data["@id"] = str(res)
+        elif '@graph' not in data:
+            data["@id"] = str(res)
 
     #reserialize
     data = json.dumps(data, indent=4)
