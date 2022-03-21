5f6ebf01f2f4eaa6df066dab2d61bf72048f9a1b
Maarten van Gompel <proycon@anaproy.nl>
2022-03-21 13:54:22 +0100

    author parsing: skip comments

--- codemeta/parsers/authors.py
+++ codemeta/parsers/authors.py
@@ -11,4 +11,5 @@ def parse_authors(g: Graph, res: Union[URIRef, BNode], file: IO, args: AttribDic
     """Parses a plain-text file of people, one person (author/contributor/maintainer) per line, may additionally contain e-mail addresses <> and (urls), in that order"""
     for i, line in enumerate(file.readlines()):
         line = line.strip()
-        add_authors(g, res, line, single_author=True, baseuri=args.baseuri, property=property, skip_duplicates=True, position=i+1)
+        if line and line[0] != "#":
+            add_authors(g, res, line, single_author=True, baseuri=args.baseuri, property=property, skip_duplicates=True, position=i+1)
