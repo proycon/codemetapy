9f8d4f0cea24811902c248ae9709c5f6f61b25a1
Maarten van Gompel <proycon@anaproy.nl>
2022-03-26 23:32:56 +0100

    fixes for web parsing (CLARIAH/clariah-plus#92)

--- codemeta/parsers/web.py
+++ codemeta/parsers/web.py
@@ -25,39 +25,45 @@ def detect_type(data):
 
 def get_meta(soup, *keys, default=""):
     for key in keys:
-        value = soup.find("meta", property=key, content=True)
-        if value and value.content:
-            return value.content
-        value = soup.find("meta", {"name": key }, content=True)
-        if value and value.content:
-            return value.content
+        for value in  soup.find_all("meta", property=key):
+            if value.get('content'):
+                return value.get('content')
+        for value in soup.find_all("meta", {"name": key }):
+            if value.get('content'):
+                return value.get('content')
     return default
 
 def parse_web(g: Graph, res: Union[URIRef, BNode], url, args: AttribDict) -> Union[URIRef,BNode,None]:
     r = requests.get(url, headers={ "Accept": "application/json+ld;q=1.0,application/json;q=0.9,application/x-yaml;q=0.8,application/xml;q=0.7;text/html;q=0.6;text/plain;q=0.1" })
     r.raise_for_status()
     contenttype = r.headers.get('content-type').split(';')[0].strip()
+    print(f"    Service replied with content-type {contenttype}",file=sys.stderr)
     datatype = None
     data = None
 
 
     if contenttype in ("application/json", "application/ld+json") or url.endswith(".json") or url.endswith(".jsonld"):
-        print("Parsing json...",file=sys.stderr)
+        print("    Parsing json...",file=sys.stderr)
         data = json.loads(r.text)
     elif contenttype in ("application/x-yaml", "text/yaml") or url.endswith(".yml") or url.endswith(".yaml"):
-        print("Parsing yaml...",file=sys.stderr)
+        print("    Parsing yaml...",file=sys.stderr)
         #may be OpenAPI
         with open(r.text,'r', encoding="utf-8") as f:
             data = yaml.load(f, yaml.Loader)
     elif contenttype == "text/html":
-        print("Parsing html...",file=sys.stderr)
+        print("    Parsing html...",file=sys.stderr)
         soup = BeautifulSoup(r.text, 'html.parser')
         scriptblock = soup.find("script", {"type":"application/ld+json"})
         if scriptblock:
             #Does the site provide proper JSON-LD metadata itself?
+            print("    Found a json-ld script block",file=sys.stderr)
             data = json.loads("".join(scriptblock.contents))
         else:
-            name = get_meta(soup, "og:site_name", "og:title", "twitter:title")
+            print("    Parsing site metadata",file=sys.stderr)
+            name = get_meta(soup, "schema:name", "og:site_name", "og:title", "twitter:title")
+            if not name and soup.title:
+                name = soup.title.text
+
             if args.with_stypes:
                 targetres = URIRef(generate_uri(name, baseuri=args.baseuri,prefix="webapplication"))
             else:
@@ -67,24 +73,26 @@ def parse_web(g: Graph, res: Union[URIRef, BNode], url, args: AttribDict) -> Uni
             g.add((targetres, RDF.type, SDO.WebApplication))
             g.add((targetres, SDO.url, Literal(get_meta(soup, "og:url", "url", default=url))))
 
-
-            v = get_meta(soup, "og:description", "twitter:description", "description")
+            v = get_meta(soup, "schema:description", "og:description", "twitter:description", "description")
             if v: g.add((targetres, SDO.description, Literal(v)))
 
-            v = get_meta(soup, "og:image", "twitter:image", "thumbnail")
+            v = get_meta(soup, "schema:url")
+            if v: g.add((targetres, SDO.url, Literal(v)))
+
+            v = get_meta(soup, "schema:thumbnailUrl", "og:image", "twitter:image", "thumbnail")
             if v: g.add((targetres, SDO.thumbnailUrl, Literal(v)))
 
-            v = get_meta(soup, "author")
+            v = get_meta(soup, "schema:author", "author")
             if v: g.add((targetres, SDO.author, Literal(v)))
 
-            v = get_meta(soup, "keywords")
+            v = get_meta(soup, "schema:keywords", "keywords")
             if v:
                 for item in v.split(","):
                     if item: g.add((targetres, SDO.keywords, Literal(v)))
 
             return targetres
     else:
-        print(f"Remote returned unknown contenttype: {contenttype}",file=sys.stderr)
+        print(f"    Remote returned unknown contenttype: {contenttype}",file=sys.stderr)
 
     if data:
         datatype = detect_type(data)
@@ -98,6 +106,6 @@ def parse_web(g: Graph, res: Union[URIRef, BNode], url, args: AttribDict) -> Uni
         elif datatype == 'openapi':
             raise NotImplementedError #TODO
         else:
-            print(f"Unable to detect data type of data returned by {url}",file=sys.stderr)
+            print(f"    Unable to detect data type of data returned by {url}",file=sys.stderr)
 
     return None
