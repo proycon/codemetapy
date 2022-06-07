6921436c9897146847eb2a4be243f3ae2937527c
Maarten van Gompel <proycon@anaproy.nl>
2022-06-07 11:52:28 +0200

    don't trip over having no dependencies (fixed as suggested by @nealmcb in #16)

--- codemeta/parsers/python.py
+++ codemeta/parsers/python.py
@@ -118,21 +118,22 @@ def parse_python(g: Graph, res: Union[URIRef, BNode], packagename: str, crosswal
             else:
                 print("WARNING: No translation for distutils key " + key,file=sys.stderr)
 
-    for value in pkg.requires:
-        for dependency in splitdependencies(value):
-            dependency, depversion = parsedependency(dependency.strip())
-            print(f"Found dependency {dependency} {depversion}",file=sys.stderr)
-            depres = URIRef(generate_uri(dependency+depversion.replace(' ',''),args.baseuri,"dependency")) #version number is deliberately in ID here!
-            g.add((depres, RDF.type, SDO.SoftwareApplication))
-            g.add((depres, SDO.identifier, Literal(dependency)))
-            g.add((depres, SDO.name, Literal(dependency)))
-            if args.exactplatformversion:
-                g.add((depres, SDO.runtimePlatform, Literal("Python " + str(sys.version_info.major) + "." + str(sys.version_info.minor) + "." + str(sys.version_info.micro))))
-            else:
-                g.add((depres, SDO.runtimePlatform, Literal("Python 3")))
-            if depversion:
-                g.add((depres, SDO.version, Literal(depversion)))
-            g.add((res, CODEMETA.softwareRequirements, depres))
+    if pkg.requires:
+        for value in pkg.requires:
+            for dependency in splitdependencies(value):
+                dependency, depversion = parsedependency(dependency.strip())
+                print(f"Found dependency {dependency} {depversion}",file=sys.stderr)
+                depres = URIRef(generate_uri(dependency+depversion.replace(' ',''),args.baseuri,"dependency")) #version number is deliberately in ID here!
+                g.add((depres, RDF.type, SDO.SoftwareApplication))
+                g.add((depres, SDO.identifier, Literal(dependency)))
+                g.add((depres, SDO.name, Literal(dependency)))
+                if args.exactplatformversion:
+                    g.add((depres, SDO.runtimePlatform, Literal("Python " + str(sys.version_info.major) + "." + str(sys.version_info.minor) + "." + str(sys.version_info.micro))))
+                else:
+                    g.add((depres, SDO.runtimePlatform, Literal("Python 3")))
+                if depversion:
+                    g.add((depres, SDO.version, Literal(depversion)))
+                g.add((res, CODEMETA.softwareRequirements, depres))
 
     #ensure 'identifier' is always set
     name = g.value(res, SDO.name)
