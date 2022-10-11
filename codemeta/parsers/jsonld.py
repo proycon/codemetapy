34e507e42d1c2b5e166c638fcf50b6e6907cdf4e
Maarten van Gompel <proycon@anaproy.nl>
2022-10-11 15:32:59 +0200

    fix in rewrite_context

--- codemeta/parsers/jsonld.py
+++ codemeta/parsers/jsonld.py
@@ -5,8 +5,9 @@ from typing import Union, IO
 from codemeta.common import PREFER_URIREF_PROPERTIES_SIMPLE, AttribDict, REPOSTATUS, license_to_spdx, SDO, SCHEMA_SOURCE, CODEMETA_SOURCE, SCHEMA_LOCAL_SOURCE, SCHEMA_SOURCE, CODEMETA_LOCAL_SOURCE, CODEMETA_SOURCE, STYPE_SOURCE, STYPE_LOCAL_SOURCE, IODATA_SOURCE, IODATA_LOCAL_SOURCE, init_context, SINGULAR_PROPERTIES, merge_graphs, generate_uri, bind_graph
 
 
-def rewrite_context(context: Union[list,str]) -> list:
+def rewrite_context(context: Union[list,str], args: AttribDict) -> list:
     """Rewrite remote contexts to their local counterparts"""
+    local_contexts = [ x[0] for x in init_context(False, args.addcontext) ]
     if isinstance(context, list):
         for i, v in enumerate(context):
             if isinstance(v, str):
@@ -18,7 +19,7 @@ def rewrite_context(context: Union[list,str]) -> list:
                     context[i] = STYPE_LOCAL_SOURCE
                 elif v.startswith(IODATA_LOCAL_SOURCE):
                     context[i] = IODATA_LOCAL_SOURCE
-                elif v.startswith(("file://","//")) and v not in (SCHEMA_LOCAL_SOURCE, CODEMETA_LOCAL_SOURCE, STYPE_LOCAL_SOURCE, IODATA_LOCAL_SOURCE):
+                elif v.startswith(("file://","//")) and v not in local_contexts:
                     raise Exception(f"Refusing to load non-authorized local context: {v}")
 
         #remove some legacy contexts which we may encounter but would choke on if parsed
@@ -27,7 +28,7 @@ def rewrite_context(context: Union[list,str]) -> list:
         except ValueError:
             pass
     elif isinstance(context, str):
-        context = rewrite_context([context])
+        context = rewrite_context([context], args)
     #ammend context
     if SCHEMA_LOCAL_SOURCE not in context:
         context.append(SCHEMA_LOCAL_SOURCE)
@@ -92,7 +93,7 @@ def parse_jsonld_data(g: Graph, res: Union[BNode, URIRef,None], data: dict, args
         print("    NOTE: Not a valid JSON-LD document, @context missing! Attempting to inject automatically...", file=sys.stderr)
 
     #rewrite context using the local schemas
-    data['@context'] = rewrite_context(data['@context'])
+    data['@context'] = rewrite_context(data['@context'], args)
 
     #convert schemeless *absolute* URLs (//, otherwise rdflib will interpret them as local). URLs starting with a single slash will be left untouched
     data = rewrite_schemeless_uri(data)
