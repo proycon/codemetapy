5f6277cf2185464ba2a615c5226e548ec6b83b0b
Maarten van Gompel <proycon@anaproy.nl>
2023-02-23 14:33:35 +0100

    fix

--- codemeta/parsers/nodejs.py
+++ codemeta/parsers/nodejs.py
@@ -28,7 +28,7 @@ def parse_nodejs(g: Graph, res: Union[URIRef, BNode], file: IO , crosswalk, args
             if key == 'name' and value:
                 #remove the 'scope' from the name
                 if value[0] == '@' and value.find('/') != -1:
-                    value = value.strip('/')[1]
+                    value = value.split('/')[1]
                 add_triple(g, res, key, value, args)
             elif key == 'bugs':
                 if isinstance(value,dict) and 'url' in value:
