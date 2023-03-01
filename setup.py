fcc2c5f06939ef3a2860239fc3177b3983d6389a
Maarten van Gompel <proycon@anaproy.nl>
2023-03-01 13:43:14 +0100

    minor metadata update

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: POSIX",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
