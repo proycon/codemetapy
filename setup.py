e444c672299b8b484aaa6732fb5137713d970259
Maarten van Gompel <proycon@anaproy.nl>
2024-06-14 11:45:48 +0200

    add Python 3.12 as supported #50

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: POSIX",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
